b7ee1ec6bd400c5b1693cff6c4aebfc4e6417d96
Pyratic <Pyratic@bitbucket.org>
2014-04-02 20:25:24 -0600

    ItemManager Panel Done*
    Added the majority of the functionality to the ItemManagerPanel
    + Currently all buttons work
    - Still needs to be merged with Scene Manager changes for linking.

--- src/Panels/ItemManagerPanel.java
+++ src/Panels/ItemManagerPanel.java
@@ -5,7 +5,7 @@ import Game_System.Item;
 import Scene_Manager.SceneManager;
 import TableModels.ItemTableModel;
 import UserIO.WindowComm;
-import Windows.EditItemWindow;
+import Windows.*;
 import Windows.MainWindow;
 import net.miginfocom.swing.MigLayout;
 import java.awt.*;
@@ -27,6 +27,9 @@ public class ItemManagerPanel extends JPanel {
 	private WindowComm 		m_WindowComm;
 	private SceneManager 	m_SceneManager;
 	private ItemTableModel 	m_ItemTableModel;
+	
+	// Constants
+	private static final int YES	= 0;
 	
 	public class ItemManagerButtonHandler implements ActionListener {
 		private ItemManagerPanel parent;
@@ -74,7 +77,7 @@ public class ItemManagerPanel extends JPanel {
 		m_RmvItmBtn.addActionListener(btnHandler);
 		add(m_RmvItmBtn);
 		
-		m_EdtItmBtn = new JButton("Edit Scene");
+		m_EdtItmBtn = new JButton("Edit Item");
 		m_EdtItmBtn.setToolTipText("Edit the selected scene.");
 		m_EdtItmBtn.setBounds(456, 75, 103, 23);
 		m_EdtItmBtn.addActionListener(btnHandler);
@@ -95,42 +98,118 @@ public class ItemManagerPanel extends JPanel {
 
 	}
 	
+	/*********************************************************************\
+	 * Contains all functionality of the Item Manager Panel				 *
+	\*********************************************************************/
+	
 	/**
-	 * Contains all functionality of the Item Manager Panel
+	 * Gets the currently selected Item.
+	 * 
+	 * @return	The Currently Selected Item in the table.
+	 */
+	private Item getSelectedItem( )
+	{
+		return m_ItemTableModel.getItemAt( m_ItemTable.getSelectedRow( ) );
+	}
+	
+	/** 
+	 * Generates a new item and opens the edit item window to modify the item.
 	 */
 	private void addItem( )
 	{
-		// m_WindowComm.displayMessage( "Add Item Button Clicked!" );
-		// TODO: program addItem functionality.
-		openEditItemWindow( null );
+		Item m_NewItem = new Item( "<Item Title>", "<Item Description>" );
+		openEditItemWindow( m_NewItem, true );
 	}
 	
 	private void removeItem( )
 	{
-		m_WindowComm.displayMessage( "Remove Item Button Clicked!" );
-		// TODO: program removeItem functionality.
+		Item m_ItemToRemove = getSelectedItem( );
+		
+		if( null != m_ItemToRemove )
+		{
+			int iResult = m_WindowComm.getYesNo( "Are you sure you want to remove \"" + 
+												 m_ItemToRemove.getName( ) + "?\"", "Remove" );
+			
+			if( YES == iResult )
+				m_ItemTableModel.removeItem( m_ItemToRemove.getName( ) );
+		}
+		else
+			m_WindowComm.displayMessage( "No Item selected!" );
 	}
 	
+	/**
+	 * Opens up the EditItemWindow with a flag to say that it's not a new item being added.
+	 * This allows the user to modify the Item with the ability to leave the name the same.
+	 * If no item is selected, this function fails.
+	 */
 	private void editItem( )
 	{
+		Item m_ItemToEdit = m_ItemTableModel.getItemAt( m_ItemTable.getSelectedRow( ) );
 		
+		if( null != m_ItemToEdit )
+			openEditItemWindow( m_ItemToEdit, false );
+		else
+			m_WindowComm.displayMessage( "No Item selected!" );
 	}
 	
-	private void openEditItemWindow(Item itemToEdit)
+	/**
+	 * Function called externally to save an item.
+	 * If the name is unique, it will skip adding the item and just fire
+	 * changes on the model.
+	 * 
+	 * @param itemToSave The Item to save.
+	 */
+	public void saveItem( Item itemToSave )
+	{
+		if( m_ItemTableModel.isNameUnique( itemToSave.getName( ) ) )
+			m_ItemTableModel.addItem( itemToSave );
+		else
+			m_ItemTableModel.fireTableDataChanged( );
+	}
+	
+	/**
+	 * Function to check to see if an Item can be added.
+	 * 
+	 * @param m_ItemToAdd	The Item to check.
+	 * @return				True if the name of the item to add is unique.
+	 */
+	public boolean canAddItem( Item m_ItemToAdd )
+	{
+		return m_ItemTableModel.isNameUnique( m_ItemToAdd.getName( ) );
+	}
+	
+	/**
+	 * Function to launch and run the edit item window.  This also sets
+	 * the buttons of this window to disabled so they can't be clicked again.
+	 * 
+	 * @param itemToEdit	The Item to load the edit item window with.
+	 * @param bNewItem		Flag to determine if the Item is a new item or not.  This
+	 * 						changes how the edit window handles the saving of the item.
+	 */
+	private void openEditItemWindow( Item itemToEdit, boolean bNewItem )
 	{
 		toggleSaveEditEnabled(false);
-		EditItemWindow esw = new EditItemWindow ( this );
+		EditItemWindow esw = new EditItemWindow ( itemToEdit, this, bNewItem );
 		esw.run();
 	}
 	
+	/**
+	 * Essentially sets the buttons to be enabled
+	 */
 	public void editItemWindowHasClosed()
 	{
 		toggleSaveEditEnabled(true);
 	}
 	
+	/** 
+	 * Enables or Disables the buttons depending on the boolean flag passed in.
+	 * 
+	 * @param b_Toggle	The Boolean flag to set the buttons with.
+	 */
 	private void toggleSaveEditEnabled(boolean b_Toggle)
 	{
 		m_AddItmBtn.setEnabled(b_Toggle);
 		m_RmvItmBtn.setEnabled(b_Toggle);
+		m_EdtItmBtn.setEnabled(b_Toggle);
 	}
 }
